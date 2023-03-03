369f54502db6a1b6a700cac64fe582f3e46720fc
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-03 22:34:26 +0900

    ✅ Add: mongooseLoader 추가

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,14 @@
 import { Express } from 'express';
 import expressLoader from './express';
 import Logger from './logger';
+import mongooseLoader from './mongoose';
 
 const loaders = async (app: Express) => {
   await expressLoader(app);
   Logger.info('✅ Express loaded');
+
+  await mongooseLoader();
+  Logger.info('✅ Mongoose loaded');
 };
 
 export default loaders;
