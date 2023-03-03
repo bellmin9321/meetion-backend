9425501f7f5f120267ac6f6ba00c4dfc5ba48297
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-03 18:38:58 +0900

    ✅ Add: prettier 설정 추가

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  parser: "typescript",
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
