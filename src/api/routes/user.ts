2add1e34d3074489201318047021f8407c9a7466
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-16 22:37:45 +0900

    ✨ Feat: user route 조희 / 추가 구현

---
+++ src/api/routes/user.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from 'express';
+import User from '../../models/User';
+const userRouter = express.Router();
+
+userRouter.get('/', async (req: Request, res: Response) => {
+  try {
+    res.status(201).json({ success: true });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      errorMessage: 'Failed to create user',
+    });
+  }
+});
+
+userRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const { email, image, name } = req.body;
+    const result = await User.findOneAndUpdate({ email }, { upsert: true });
+
+    if (!result) {
+      await User.create({
+        email,
+        image,
+        name,
+      });
+    }
+
+    res.status(201).json({ success: true });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      errorMessage: 'Failed to create user',
+    });
+  }
+});
+
+export default userRouter;
