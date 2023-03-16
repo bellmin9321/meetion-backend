565821f87f58d688654206552c1960a3f4d6597b
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-16 22:37:45 +0900

    ✨ Feat: user route 조희 / 추가 구현

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,11 +1,11 @@
 import { Express } from 'express';
 
-import usersRouter from './users';
+import userRouter from './user';
 import pagesRouter from './pages';
 import healthRouter from './health';
 
 const routesLoader = (app: Express) => {
-  app.use('/users', usersRouter);
+  app.use('/user', userRouter);
   app.use('/myPage', pagesRouter);
   app.use('/health', healthRouter);
 };
