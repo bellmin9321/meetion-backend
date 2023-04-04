6cef5dc02809a222fcb8bcee208809eead6d0bfe
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-04 15:01:56 +0900

    ✨ Feat: webRTC socket 구현

--- src/loaders/socket.ts
+++ src/loaders/socket.ts
@@ -88,6 +88,57 @@ const onSocket = (server: any) => {
         socket.broadcast.to(String(info.id)).emit('pos-changes', info);
       },
     );
+
+    // WebRTC
+    socket.on('join_room', (data) => {
+      // 방이 기존에 생성되어 있다면
+      if (users[data.room]) {
+        // 현재 입장하려는 방에 있는 인원수
+        const currentRoomLength = users[data.room].length;
+        if (currentRoomLength === MAXIMUM) {
+          // 인원수가 꽉 찼다면 돌아갑니다.
+          socket.to(socket.id).emit('room_full');
+          return;
+        }
+
+        // 여분의 자리가 있다면 해당 방 배열에 추가해줍니다.
+        users[data.room] = [...users[data.room], { id: socket.id }];
+      } else {
+        // 방이 존재하지 않다면 값을 생성하고 추가해줍시다.
+        users[data.room] = [{ id: socket.id }];
+      }
+      console.log(users);
+      socketRoom[socket.id] = data.room;
+
+      // 입장
+      socket.join(data.room);
+
+      // 입장하기 전 해당 방의 다른 유저들이 있는지 확인하고
+      // 다른 유저가 있었다면 offer-answer을 위해 알려줍니다.
+      const others = users[data.room].filter(
+        (user: any) => user.id !== socket.id,
+      );
+      console.log(others);
+
+      if (others.length) {
+        io.sockets.to(socket.id).emit('all_users', others);
+      }
+    });
+
+    socket.on('offer', (sdp, roomName) => {
+      // offer를 전달받고 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit('getOffer', sdp);
+    });
+
+    socket.on('answer', (sdp, roomName) => {
+      // answer를 전달받고 방의 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit('getAnswer', sdp);
+    });
+
+    socket.on('candidate', (candidate, roomName) => {
+      // candidate를 전달받고 방의 다른 유저들에게 전달해 줍니다.
+      socket.to(roomName).emit('getCandidate', candidate);
+    });
   });
 };
 
